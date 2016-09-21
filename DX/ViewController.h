2581c35edd4812bc5b7f5f105a16750cdf04a88c
wangaiguo <wangaiguo@wangaiguodeMac-mini.local>
2016-09-21 11:53:02 +0800

    Initial Commit

---
+++ DX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DX
+//
+//  Created by wangaiguo on 16/9/21.
+//  Copyright © 2016年 wangaiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DX/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DX
+//
+//  Created by wangaiguo on 16/9/21.
+//  Copyright © 2016年 wangaiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
